5ba740e48b1b5850c925681ebbc3c86da0b4c900
Vinícius Blandy <viny19381@hotmail.com>
2023-05-09 08:54:42 -0300

    Atualizando repositorio, adicionando validação e condição

--- main.js
+++ main.js
@@ -16,6 +16,29 @@ $(document).ready(function () {
         const soma = campoA + campoB;
         const media = soma / 2
 
+
+        if (isNaN(campoA) || isNaN(campoB)) {
+            resposta.empty();
+            return;
+        }
+
         resposta.html(`O resultado da sua soma é <strong>${soma}</strong>, e a média <strong>${media}</strong>`);
+    });
+
+
+    $('form').validate({
+        rules: {
+            campoA: {
+                required: true
+            },
+            campoB: {
+                required: true
+            }
+        },
+        messages: {
+            campoA: "Por favor insira um número",
+            campoB: "Por favor insira um número"
+        },
+        errorClass: 'error-message'
     });
 });
